9f0290c58a9bb3c87595e0f27b0db5acbd0533ec
matyas-bohunka <66198192+matyas-bohunka@users.noreply.github.com>
2020-05-31 19:53:14 +0200

    Add files via upload

---
+++ pokedex.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "a opció: ability.txt-be menti és kiírja mit csinál egy adott képesség az id-je alapján."
+echo "t opció: type.txt-be menti és kiírja hogy az adott pokémon típushoz hány pokémon tartozik id."
+echo "n opció: name.txt-be menti és kiírja a id-je alapján megkeresett pokémon nevét. "
+
+while getopts t:a:n: options; do
+ case $options in
+	t) typeid=$OPTARG
+	curl "https://pokeapi.co/api/v2/type/"$typeid | tr ',' "\n" | grep -c "pokemon" > type.txt
+	cat type.txt ;;
+	a) abilityid=$OPTARG
+	curl "https://pokeapi.co/api/v2/ability/"$abilityid | tr ',' "\n" | grep '"effect_entries":' > ability.txt
+	cat ability.txt ;;
+	n) pokeid=$OPTARG
+	curl "https://pokeapi.co/api/v2/pokemon/"$pokeid | tr ',' "\n" | grep 'forms' | cut -c 18-> name.txt
+	cat name.txt ;;
+ esac
+done
+
